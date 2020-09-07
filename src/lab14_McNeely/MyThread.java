db318120175e61b71a4cdcf03b0210dd1958032b
Garrett McNeely <garrettmcneely02@gmail.com>
2020-09-07 02:39:33 -0400

    Second commit

--- src/lab14_McNeely/MyThread.java
+++ src/lab14_McNeely/MyThread.java
@@ -1,50 +1,29 @@
 package lab14_McNeely;
 
-public class MyThread extends Thread{
-	
+public class MyThread extends Thread {
+
 	String text;
-	
+
 	public int count;
-	
-	
-	public MyThread(String text)
-	{
+
+	public MyThread(String text) {
 		this.text = text;
 	}
-	
-	  @Override
-	    public void run()
-	    {
-	        this.count = 0;
-	        char[] characters = this.text.toCharArray();
-	        
-	     
-	  /*      try
-	        {
-	        	this.sleep(5000);
-	        }
-	    	catch (InterruptedException ex)
-	        {
-	    		ex.printStackTrace();
-	        }
-     */
-	        
 
-	        for(int i = 0; i < characters.length - 3; i++)
-	        {
-	            if(characters[i] == 't')
-	            {
-	                if(characters[i + 1] == 'h')
-	                {
-	                    if(characters[i + 2] == 'e')
-	                    {
-	                        this.count++;
-	                    }
-	                }
-	            }
-	        }
-	    }
+	@Override
+	public void run() {
+		this.count = 0;
+		char[] characters = this.text.toCharArray();
 
-	
+		for (int i = 0; i < characters.length - 3; i++) {
+			if (characters[i] == 't') {
+				if (characters[i + 1] == 'h') {
+					if (characters[i + 2] == 'e') {
+						this.count++;
+					}
+				}
+			}
+		}
+	}
 
 }
